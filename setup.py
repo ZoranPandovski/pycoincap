5544bef0f09a182cd339237a93968e2712ac0907
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-10-03 20:47:20 +0200

    Initial structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='pycoincap',
+   version='1.0',
+   description='Module for getting cryptocurrency data from Coinmarketcap',
+   author='Zoran Pandovski',
+   author_email='zoran.pandovski@gmail.com',
+   url='https://github.com/anfederico/Stocktalk',
+   packages=['pycoincap'],
+   install_requires=['']
+)
